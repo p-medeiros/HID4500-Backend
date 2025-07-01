995928cec009dd502eb8cb5bddc42287ebc005ee
Pedro Medeiros <pmfa2805@gmail.com>
2025-06-30 23:34:23 -0300

    fix cors

--- src/express.ts
+++ src/express.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import fingerprintRoutes from './routes/fingerprintRoutes';
 import path from 'path';
 import fs from 'fs';
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json({ limit: '10mb' }));
+app.use(cors({maxAge : 600}));
 
 // Garante que a pasta exista
 const fingerprintDir = path.join(__dirname, 'fingerprints');
